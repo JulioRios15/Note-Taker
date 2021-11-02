c37099b8f81c4bf633e2c6966a2b52bc09c27f26
Julio Rios <rios.julio.david@outlook.com>
2021-11-02 19:26:09 -0400

    initial routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import http from 'http';
+import path from 'path';
 
-const port = 3500;
+const PORT = 3500;
+const __dirname = path.resolve(path.dirname(''));
 
 // Initialize the server app
 const app = express();
@@ -10,12 +12,22 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+//Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  }); 
+   
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });  
+
+
 //Create the server
 const httpServer = http.createServer(app);
 
 //Start server
-httpServer.listen(port, 
+httpServer.listen(PORT, 
     async () => {
     // Log to the console
-    console.log(`server running on port:${port}`);
+    console.log(`server running on port:${PORT}`);
 });
