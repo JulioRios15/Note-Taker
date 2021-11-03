62cb7e7de486e8109719dad723cf2edab70bba0d
Julio Rios <rios.julio.david@outlook.com>
2021-11-02 21:27:35 -0400

    note routes

--- Develop/routes/notesRoutes.js
+++ Develop/routes/notesRoutes.js
@@ -1,18 +1,30 @@
 import express from 'express';
+import utils from '../utils/utils.js'
+
 //Initialize the router
 const router = express.Router();
 
 //Route requests
 router.get('/', (req, res) => {
+    const jsonDB = utils.getJsonDB();
+    console.log(jsonDB);
 
+    if(jsonDB){
+        return res.status(200).json(jsonDB);
+    } else{
+        return res.status(404).json({
+            messege: "unable to find notes"
+        })
+    }
+    
 });
 
 router.post('/', (req, res) => {
-
+    console.log(req.body);
 });
 
 router.delete('/:id', (req, res) => {
-    
+
 });
 
 
